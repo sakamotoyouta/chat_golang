0b055c355a1efb85c3d7e1ef54cab2aad3c2935b
龍太 <ryouta@ryuuta-no-MacBook-Pro.local>
2018-12-10 23:57:50 +0900

    googleのログイン機能を追加

--- golang_chat/main.go
+++ golang_chat/main.go
@@ -36,11 +36,11 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈します
 	// Gomniauthのセットアップ
-	gomniauth.SetSecurityKey("セキュリティキー") // 自分で決めた文字列
+	gomniauth.SetSecurityKey("hogefugahogefuga") // 自分で決めた文字列
 	gomniauth.WithProviders(
 		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
 		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
-		google.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/google"),
+		google.New(GoogleAuth["clientId"], GoogleAuth["secretKey"], "http://localhost:8080/auth/callback/google"),
 	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
